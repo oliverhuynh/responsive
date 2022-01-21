e34c24875e6c077ca82759ab25037376c28170f6
Oliver Huynh <oliver@jufist.com>
2022-01-21 22:47:38 +0700

    More viewports

--- src/responsive.js
+++ src/responsive.js
@@ -1,13 +1,24 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.haclaresponsive = {
     attach: function attach(context) {
+      const viewports = drupalSettings.viewports || {
+        'toMobile': [0, 1080, 'is-mobile'],
+        'toWeb': [1080, 1111111, 'is-web']
+      };
       const refresh = Drupal.debounce(() => {
-        if ($(window).width() < 1080) {
-          $(window).trigger('toMobile');
-          $('html').addClass('is-mobile');
-        } else {
-          $(window).trigger('toWeb');
-          $('html').removeClass('is-mobile');
+        const w = $(window).width();
+        // Clear
+        for (const vp of Object.keys(viewports)) {
+          const [min, max, cl] = viewports[vp];
+          $('html').removeClass(cl);
+        }
+
+        for (const vp of Object.keys(viewports)) {
+          const [min, max, cl] = viewports[vp];
+          if (w < max && w >= min) {
+            $(window).trigger(vp);
+            $('html').addClass(cl);
+          }
         }
       }, 250);
       $(window).once('res').resize(refresh);
